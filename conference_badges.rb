4849586050cd0037b5107309416802638dc0dc85
Allison Johnson <allison.cuttler@gmail.com>
2019-12-09 19:13:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,4 +9,14 @@ def batch_badge_creator(names)
     messages << message
   end #each
   messages
-end
+end #method batch_badge_creator
+
+def assign_rooms(speakers)
+  rooms = Array.new 
+  room_num = 1 
+  speakers.each do |speaker|
+    rooms << "Hello, #{speaker}! You'll be assigned to room #{room_num}!"
+    room_num += 1 
+  end #each
+  rooms
+end #method assign_rooms
