777315e9d31045d3d85d91506ef01e3bf30609f1
Allison Johnson <allison.cuttler@gmail.com>
2019-12-09 19:08:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,4 +2,11 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end #method badge_maker
 
-batch
+batch_badge_creator(names)
+  messages = Array.new
+  names.each do |n|
+    message = badge_maker(n)
+    messages << message
+  end #each
+  messages
+end
